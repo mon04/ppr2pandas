ed5f2442d705f223ce890cbfb3d1485fd583d126
Michael <michaelon024@gmail.com>
2024-07-15 23:45:37 +0100

    Remove unnecessary setUp method

--- tests/test__dl.py
+++ tests/test__dl.py
@@ -6,10 +6,6 @@ from ppr2pandas._dl import download_ppr_all_csv, download_ppr_specific_csv
 
 class TestDl(unittest.TestCase):
 
-    def setUp(self):
-        logging.basicConfig(level=logging.DEBUG)
-        self.logger = logging.getLogger()
-
     def test_download_prr_all_csv(self):
         data = download_ppr_all_csv()
         self.assertTrue(len(data) > 0)
